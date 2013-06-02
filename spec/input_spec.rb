c23db7bee1f9e56c068a407273e0c69fa777eb5a
Ben Snape <ben.snape@itv.com>
2013-06-02 21:02:31 +0100

    tidied up the spec file

--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -2,14 +2,16 @@ require_relative '../lib/input'
 
 describe Input do
 
-  it 'should take a text file as input' do
+  before(:each) do
     @input = Input.new
+  end
+
+  it 'should take a text file as input' do
     file = File.new('tmp/file.txt', 'w+')
     @input.read_input file
   end
 
   it 'should store the input in an instance variable' do
-    @input = Input.new
     file = File.new('spec/sudoku.txt', 'r')
     @input.read_input file
     @input.input.should_not be_nil
